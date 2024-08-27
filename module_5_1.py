721c1089192b71c23ebd1680fa1ad67c58a096e2
LVPerfileva <79102134098@ya.ru>
2024-08-27 20:05:40 +0300

    Add files via upload
    Исправленный файл

--- module_5_1.py
+++ module_5_1.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        if new_floor <= self.number_of_floors >= 1:
+        if 1 <= new_floor <= self.number_of_floors :
             for i in range(new_floor):
                 print(i + 1)
         else:
@@ -15,3 +15,5 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+h1.go_to(-5)
+h2.go_to(0)
